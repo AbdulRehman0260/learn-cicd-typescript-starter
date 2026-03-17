dfb67389b36a280825f5f079ef4530c044f3f23c
Abdul Rehman Shoukat <abdulzain19@gmail.com>
2026-03-17 10:10:45 +0000

    pushing broken code to see effectd

--- src/tests/apiTest.test.ts
+++ src/tests/apiTest.test.ts
@@ -4,6 +4,6 @@ import { getAPIKey } from "../api/auth.js";
 
 describe("getAPIKey", () => {
     test("apiKey is defined", () => {
-        expect(getAPIKey).toBeUndefined();
+        expect(getAPIKey).toContain("hello");
     });
 });
